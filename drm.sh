4f465c04133abc298ad0556be783942e91597090
Michael Kim <michael.kim.200518@gmail.com>
2021-07-14 14:53:09 +0900

    Upgrade to Node 6

--- drm.sh
+++ drm.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+rm -rf agora/node/2/.cache
+rm -rf agora/node/3/.cache
+rm -rf agora/node/4/.cache
+rm -rf agora/node/5/.cache
+rm -rf agora/node/6/.cache
+rm -rf agora/node/7/.cache
 rm -rf agora/.cache
 rm -rf agora/log
 rm -rf stoa/data
